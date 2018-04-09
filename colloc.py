9c67046086aa19ad2976e1551ccf587ddacca38d
ikondyurin <38222960+ikondyurin@users.noreply.github.com>
2018-04-09 20:21:20 +0300

    Update colloc.py

--- colloc.py
+++ colloc.py
@@ -5,24 +5,24 @@ import operator
 import string
 import math
 
-# токенизация
+#tokenisation
 l = []
 with open("book.txt", 'r', encoding="utf-8") as f:
     for line in f:
         for w in nltk.word_tokenize(line.lower()):
             if w not in string.punctuation:
                 l.append(w)
-# создание частотного списка
+#frequency list
 freq = nltk.FreqDist(l)
-# сортировка частотного списка по частоте
+#sorting
 sort_fr = freq.most_common()
 
-#запись в файл
+#writing into file
 with open("freq.csv", 'w', encoding="windows-1251") as freq_d:
     for i, pair in enumerate(sort_fr):
         freq_d.write("{}; {}; {}; {}; {}\n".format(pair[0], pair[1], i+1, round(math.log1p(pair[1]),2), round(math.log1p(i+1),2)))
 
-# определение коллокаций
+#building lists of collocations using four measures
 measures = BigramAssocMeasures()
 finder = BigramCollocationFinder.from_words(l)
 t = finder.nbest(measures.student_t, 100)
@@ -30,11 +30,19 @@ c = finder.nbest(measures.chi_sq, 100)
 l = finder.nbest(measures.likelihood_ratio, 100)
 p = finder.nbest(measures.pmi, 100)
 
+#
+#
+#
+#
+#
+#to observe the problem try this:
 """
 for q in p:
     print(q)
     print(finder.score_ngram(measures.pmi, n[0], n[1]))
 """
+
+#writing collocations and values into .csv file
 with open("collocations.csv", 'w', encoding="windows-1251") as coll:
     for i in t:
         coll.write("{}; {}; {}\n".format("t", i[0]+" "+i[1], round(finder.score_ngram(measures.student_t, i[0], i[1]),2)))
